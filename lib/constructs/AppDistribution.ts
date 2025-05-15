dfffbb71db4ccfd8d6843317793160940002b72b
vberkoz <vberkoz@gmail.com>
2025-05-15 19:10:59 +0300

    refactor: move API-related distribution logic to AppDistribution
    The API-related distribution logic was removed from SiteDistribution and moved to a new AppDistribution construct to improve code organization and separation of concerns. This change ensures that each distribution construct has a single responsibility, making the codebase more maintainable and easier to understand.

--- lib/constructs/AppDistribution.ts
+++ lib/constructs/AppDistribution.ts
@@ -4,27 +4,38 @@ import {
     aws_cloudfront as cloudfront,
     aws_cloudfront_origins as origins,
     aws_certificatemanager as acm,
+    aws_apigatewayv2 as apigateway,
     Duration,
 } from 'aws-cdk-lib';
 
-export interface SiteDistributionProps {
-    siteBucket: s3.Bucket;
+export interface AppDistributionProps {
+    appBucket: s3.Bucket;
     certificate: acm.Certificate;
     domainName: string;
+    api: apigateway.HttpApi;
     region: string;
 }
 
-export class SiteDistribution extends Construct {
+export class AppDistribution extends Construct {
     public readonly distribution: cloudfront.Distribution;
 
-    constructor(scope: Construct, id: string, props: SiteDistributionProps) {
+    constructor(scope: Construct, id: string, props: AppDistributionProps) {
         super(scope, id);
 
         this.distribution = new cloudfront.Distribution(this, 'Distribution', {
             defaultRootObject: 'index.html',
             defaultBehavior: {
-                origin: origins.S3BucketOrigin.withOriginAccessControl(props.siteBucket),
+                origin: origins.S3BucketOrigin.withOriginAccessControl(props.appBucket),
                 viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+            },
+            additionalBehaviors: {
+                'api/*': {
+                    origin: new origins.HttpOrigin(`${props.api.apiId}.execute-api.${props.region}.amazonaws.com`),
+                    viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                    allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+                    cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
+                    originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER,
+                },
             },
             domainNames: [props.domainName],
             certificate: props.certificate,
